a6f72fce71ce2f4948f4d24efac4c7dbd4b7e32d
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-07 00:36:37 -0300

    testing inline components, i dont like it

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-persons',
-  templateUrl: './persons.component.html',
+  template: `<h1>Persons List</h1>
+    <app-person></app-person>
+    <app-person></app-person>`,
 })
 export class PersonsComponent {}
