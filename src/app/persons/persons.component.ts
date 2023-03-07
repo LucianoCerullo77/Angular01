171a9060aea12c656a45af849ecd4471ecb18ce7
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-07 12:13:24 -0300

    bootstrap styles imported and running on app

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-persons',
-  template: `<h1>Persons List</h1>
-    <app-person></app-person>
-    <app-person></app-person>`,
+  templateUrl: './persons.Component.html',
+  styleUrls:['./persons.component.css']
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: blue;
+  //     }
+  //   `,
+  // ],
 })
 export class PersonsComponent {}
